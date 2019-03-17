ca1923099fcf19f4db6b700c48fce1e4d65ebe68
dgm <vsl_sv@bk.ru>
2019-03-17 20:45:55 +0300

    with comments

--- src/index.js
+++ src/index.js
@@ -3,5 +3,49 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  return 4;
+
+	var firstElement, secondElement, thirdElement;
+	var result = 0;
+
+	for (var i = 0; i < preferences.length; i++) {
+		
+		firstElement = preferences[i]; 
+
+		secondElement = preferences[firstElement-1];
+		
+		thirdElement = preferences[secondElement-1];
+
+		//console.log (preferences, preferences[i] );
+		//console.log (" I = " + i + ". firstElement = " + firstElement + ". secondElement = " + secondElement + ". thirdElement = " + thirdElement + "\n"  );
+		
+
+
+
+		if ((thirdElement == i+1) && (firstElement < secondElement) && (thirdElement < secondElement))
+		{
+
+			
+
+
+			result = result + 1;
+
+		//	console.log("result = " + result )
+		}
+	}
+
+
+
+
+
+
+	//console.log(preferences + "\n");
+
+ // console.log (result);
+  return result;
 };
+
+
+
+//if (true == 0) {
+//	preferences.length //общее число элементов
+//};
